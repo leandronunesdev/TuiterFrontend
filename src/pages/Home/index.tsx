4c92d106f15895de019b4ca82fe122369e395de5
Leandro <nunes.pessoal@gmail.com>
2025-07-22 19:49:18 -0300

    implmented home file

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import Main from "../../components/Main";
+import Section from "../../components/Section";
+
+const Home = () => {
+  return (
+    <Main>
+      <Section aria-label="Home page">
+        <h1>Tuiter</h1>
+        <p>Welcome to Tuiter!</p>
+      </Section>
+    </Main>
+  );
+};
+
+export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import CreateAccount from "../pages/CreateAccount";
+import Home from "../pages/Home";
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
