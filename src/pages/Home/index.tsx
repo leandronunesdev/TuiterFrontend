64325fc8885964df842ee643bdd290320e40a544
Leandro <nunes.pessoal@gmail.com>
2025-07-31 19:47:09 -0300

    implements header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,14 +2,18 @@ import Main from "../../components/Main";
 import UserProfileSidebar from "./components/UserProfileSidebar";
 import RightSidebar from "./components/RightSidebar";
 import UserFeedContent from "./components/UserFeedContent";
+import Header from "../../components/Header";
 
 const Home = () => {
   return (
-    <Main>
-      <UserProfileSidebar />
-      <UserFeedContent />
-      <RightSidebar />
-    </Main>
+    <>
+      <Header />
+      <Main>
+        <UserProfileSidebar />
+        <UserFeedContent />
+        <RightSidebar />
+      </Main>
+    </>
   );
 };
 
